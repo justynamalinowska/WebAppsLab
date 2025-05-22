6633f5cdfe92a1a512ab9562c5ee499f96242364
Justyna Malinowska <justynamalinowska758@gmail.com>
2025-05-22 22:53:31 +0200

    Add database

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440f0d62d3500c8288ab16b5b962871d04a61fd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+018f6d3ea9e41fa739da2da532175cc260b6e283")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
